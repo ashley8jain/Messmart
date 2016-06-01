4b5150493d760d81840bacbcc97f9ef178760417
ashleyjain <ashley8jain@gmail.com>
2016-06-01 17:21:48 +0530

    modified startactivity activity and added animation for translation of element

--- app/src/main/java/com/ashleyjain/messmart/Fragment/ViewpagerFragment.java
+++ app/src/main/java/com/ashleyjain/messmart/Fragment/ViewpagerFragment.java
@@ -4,27 +4,22 @@ package com.ashleyjain.messmart.Fragment;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.ashleyjain.messmart.adapter.CustomSwipeAdapter;
+import com.ashleyjain.messmart.MessListTabLayout;
 import com.ashleyjain.messmart.R;
-
-import java.util.Timer;
-import java.util.TimerTask;
+import com.ashleyjain.messmart.StartActivity;
 
 
 public class ViewpagerFragment extends Fragment {
 
-    ViewPager viewPager;
-    CustomSwipeAdapter adapter;
-    Timer swipeTimer;
-    int currentPage = 0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -38,29 +33,39 @@ public class ViewpagerFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        viewPager = (ViewPager) view.findViewById(R.id.view_pager);
-        adapter = new CustomSwipeAdapter(getActivity());
-        viewPager.setAdapter(adapter);
+        TranslateAnimation moveToptoBottom = new TranslateAnimation(0,0, -1000, 0);
+        moveToptoBottom.setDuration(2000);
+        moveToptoBottom.setFillAfter(true);
 
-        final Handler handler = new Handler();
+        TranslateAnimation moveBottomtoTop = new TranslateAnimation(0,0,1000,0);
+        moveBottomtoTop.setDuration(2000);
+        moveBottomtoTop.setFillAfter(true);
 
-        final Runnable Update = new Runnable() {
-            public void run() {
-                if (currentPage == 4) {
-                    currentPage = 0;
-                }
-                viewPager.setCurrentItem(currentPage++, true);
-            }
-        };
+        TranslateAnimation moveLefttoRight = new TranslateAnimation(-900,0, 0, 0);
+        moveLefttoRight.setDuration(2000);
+        moveLefttoRight.setFillAfter(true);
 
-        swipeTimer = new Timer();
-        swipeTimer.schedule(new TimerTask() {
+        TextView welcome = (TextView) view.findViewById(R.id.welcome);
+        TextView place = (TextView) view.findViewById(R.id.place);
+        welcome.setAnimation(moveToptoBottom);
+        place.setAnimation(moveBottomtoTop);
 
+        Button bookmeal = (Button) view.findViewById(R.id.bookmeal);
+        bookmeal.setAnimation(moveLefttoRight);
+        bookmeal.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                handler.post(Update);
+            public void onClick(View v) {
+                MessListTabLayout fragment = new MessListTabLayout("", "", false);
+                Bundle bundle3 = new Bundle();
+                bundle3.putString("days", StartActivity.days.toString());
+                bundle3.putString("days2", StartActivity.days2.toString());
+                fragment.setArguments(bundle3);
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_not, fragment, fragment.toString())
+                        .addToBackStack(fragment.toString())
+                        .commit();
             }
-        }, 1000, 3000);
+        });
 
         ImageView imgFacebook = (ImageView)view.findViewById(R.id.imageViewFacebook);
         imgFacebook.setOnClickListener(new View.OnClickListener(){
