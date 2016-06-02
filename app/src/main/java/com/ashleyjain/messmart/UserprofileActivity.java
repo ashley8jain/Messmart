a6f8711d60073b298bd2f6ccdb205c7d7057dca5
ashleyjain <ashley8jain@gmail.com>
2016-06-02 13:47:50 +0530

    refresh automatically and removed toast when editing profile

--- app/src/main/java/com/ashleyjain/messmart/UserprofileActivity.java
+++ app/src/main/java/com/ashleyjain/messmart/UserprofileActivity.java
@@ -2,6 +2,7 @@ package com.ashleyjain.messmart;
 
         import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -28,7 +29,6 @@ import java.util.Map;
 
 public class UserprofileActivity extends Fragment {
     ImageView userlogo;
-    static int uid;
     static TextView inputUsername1,inputUserwallet1,inputUseraddress1;
     String name,address;
 
@@ -47,7 +47,7 @@ public class UserprofileActivity extends Fragment {
         inputUsername1 = (TextView)view.findViewById(R.id.userinputname1);
         inputUseraddress1 = (TextView)view.findViewById(R.id.userinputaddress1);
         inputUserwallet1 = (TextView)view.findViewById(R.id.userinputwallet1);
-        getIdUser();
+
         setInfo();
 
         TextView showDialogedit = (TextView) view.findViewById(R.id.showdialogedit);
@@ -69,27 +69,50 @@ public class UserprofileActivity extends Fragment {
 
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                //userinputtext.setText(userInput.getText());
-                                String url = "http://192.168.0.106/mess/index.php/ajaxactions";
+                                final ProgressDialog dialog2 = ProgressDialog.show(getActivity(), "", "Loading......", true);
+                                String url = StartActivity.host+"index.php/ajaxactions";
                                 StringRequestCookies postRequest = new StringRequestCookies(Request.Method.POST, url,
                                         new Response.Listener<String>() {
                                             @Override
                                             public void onResponse(String response) {
-
-                                                Toast.makeText(getActivity(),response,Toast.LENGTH_LONG).show();
-
+                                                Log.d("Response", response);
+                                                //response JSON from url
+                                                try {
+                                                    JSONObject jsonResponse = new JSONObject(response);
+                                                    Integer ec = jsonResponse.getInt("ec");
+                                                    if (ec == 1) {
+                                                        StartActivity.get().getSupportFragmentManager().popBackStack();
+                                                        UserprofileActivity userprofileActivity = new UserprofileActivity();
+                                                        StartActivity.get().getSupportFragmentManager().beginTransaction()
+                                                                .replace(R.id.fragment_not, userprofileActivity, userprofileActivity.toString())
+                                                                .addToBackStack(userprofileActivity.toString())
+                                                                .commit();
+                                                    } else {
+                                                        Toast.makeText(getActivity(), StartActivity.errorcode.getString("" + ec), Toast.LENGTH_LONG).show();
+                                                    }
+                                                    System.out.println("Message: " + ec);
+                                                    dialog2.dismiss();
+                                                } catch (JSONException e) {
+                                                    Toast.makeText(getActivity(), e.toString(), Toast.LENGTH_LONG).show();
+                                                    dialog2.dismiss();
+                                                }
                                             }
+                                            //Toast.makeText(getActivity(),response,Toast.LENGTH_LONG).show();
                                         },
-                                        new Response.ErrorListener() {
+                                        new Response.ErrorListener()
+
+                                        {
 
                                             @Override
                                             public void onErrorResponse(VolleyError error) {
                                                 Toast.makeText(getActivity(), error.toString(), Toast.LENGTH_LONG).show();
-                                                //dialog.dismiss();
+                                                dialog2.dismiss();
                                             }
                                         }
 
-                                ) {
+                                )
+
+                                {
                                     @Override
                                     protected Map<String, String> getParams() {
                                         Log.d("debug", "posting param");
@@ -98,14 +121,20 @@ public class UserprofileActivity extends Fragment {
                                         // the POST parameters:
                                         params.put("action", "saveprofile");
                                         params.put("name", userInputname.getText().toString());
-                                        params.put("address",userInputaddress.getText().toString());
+                                        params.put("address", userInputaddress.getText().toString());
                                         System.out.println(params);
                                         return params;
                                     }
                                 };
 
                                 // add it to the RequestQueue
-                                StartActivity.get().getRequestQueue().add(postRequest);
+                                StartActivity.get().
+
+                                        getRequestQueue()
+
+                                        .
+
+                                                add(postRequest);
 
 
                             }
@@ -116,59 +145,10 @@ public class UserprofileActivity extends Fragment {
         });
     }
 
-    private void getIdUser(){
-        int id =0;
-        String url = "http://192.168.0.106/mess/index.php/ajaxactions";
-        StringRequestCookies postRequest = new StringRequestCookies(Request.Method.POST, url,
-                new Response.Listener<String>() {
-                    @Override
-
-                    public void onResponse(String response) {
-                        try{
-                            JSONObject jsonObject = new JSONObject(response);
-                            JSONObject data = jsonObject.getJSONObject("data");
-                            JSONObject drawer = data.getJSONObject("drawer");
-                            UserprofileActivity.uid = drawer.getInt("loginid");
-                            Toast.makeText(getActivity(),UserprofileActivity.uid +"",Toast.LENGTH_LONG).show();
-
-                        }
-                        catch(JSONException je){
-                            Toast.makeText(getActivity(),je.toString(),Toast.LENGTH_LONG).show();
-                        }
-
-
-                    }
-                },
-                new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(getActivity(), error.toString(), Toast.LENGTH_LONG).show();
-                        //dialog.dismiss();
-                    }
-                }
-
-        ) {
-            @Override
-            protected Map<String, String> getParams() {
-                Log.d("debug", "posting param");
-                Map<String, String> params = new HashMap<String, String>();
-
-                // the POST parameters:
-                params.put("action", "getinit");
-                System.out.println(params);
-                return params;
-            }
-        };
-
-        // add it to the RequestQueue
-        StartActivity.get().getRequestQueue().add(postRequest);
-
-    }
-
 
     private void setInfo(){
-        String url = "http://192.168.0.106/mess/index.php/ajaxactions";
+        final ProgressDialog dialog = ProgressDialog.show(getActivity(), "", "Loading...", true);
+        String url = StartActivity.host+"index.php/ajaxactions";
         StringRequestCookies postRequest = new StringRequestCookies(Request.Method.POST, url,
                 new Response.Listener<String>() {
                     @Override
@@ -183,11 +163,12 @@ public class UserprofileActivity extends Fragment {
                             inputUsername1.setText(name);
                             inputUseraddress1.setText(address);
                             inputUserwallet1.setText(uinfo.getString("wallet")+"/-");
-                            Picasso.with(getActivity()).load("http://192.168.0.106/mess/" + uinfo.getString("profilepic")).into(userlogo);
-
+                            Picasso.with(getActivity()).load(StartActivity.host + uinfo.getString("profilepic")).into(userlogo);
+                            dialog.dismiss();
                         }
                         catch(JSONException je){
                             Toast.makeText(getActivity(),je.toString(),Toast.LENGTH_LONG).show();
+                            dialog.dismiss();
                         }
 
 
@@ -198,7 +179,7 @@ public class UserprofileActivity extends Fragment {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         Toast.makeText(getActivity(), error.toString(), Toast.LENGTH_LONG).show();
-                        //dialog.dismiss();
+                        dialog.dismiss();
                     }
                 }
 
@@ -210,7 +191,7 @@ public class UserprofileActivity extends Fragment {
 
                 // the POST parameters:
                 params.put("action", "profile");
-                params.put("uid", UserprofileActivity.uid+"");
+                params.put("uid",StartActivity.loginid+"");
                 System.out.println(params);
                 return params;
             }
